31b9b2e8eef42ae0ad4913d341abf19f6a6f9dd6
David Gustafsson <glasklas@gmail.com>
2021-04-22 16:54:59 +0200

    Initial commit

---
+++ run_updated_borrowers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="/home/koha/papercut"
+EOP_FILE="eop_users.txt"
+PC_FILE="pc_users.txt"
+
+cd "$DIR"
+rm -f "$EOP_FILE"
+rm -f "$PC_FILE"
+
+/usr/sbin/koha-shell koha -c "perl updated_borrowers.pl"
+
+if test -f "$EOP_FILE"
+then
+    scp "$EOP_FILE" root@intra.ub.gu.se:/opt/eop_update/data/last_updated_eop.tab
+    ssh root@intra.ub.gu.se "/opt/eop_update/run_update_users_email.sh"
+fi
+
+if test -f "$PC_FILE"
+then
+    scp "$PC_FILE" root@intra.ub.gu.se:/opt/load_pc_user/data/new_users
+    ssh root@intra.ub.gu.se "/opt/load_pc_user/run_load_new_users.sh"
+fi
---
+++ updated_borrowers.pl
@@ -0,0 +1,66 @@
+BEGIN {
+    # find Koha's Perl modules
+    # test carefully before changing this
+    use FindBin;
+    eval { require "$FindBin::Bin/../kohalib.pl" };
+}
+
+use Koha::Patron;
+use Koha::Patrons;
+use Koha::DateUtils;
+
+my $dtf = Koha::Database->new->schema->storage->datetime_parser;
+my $date = dt_from_string();
+$date->subtract(hours => 1);
+
+# EOP: borrowernumber cardnumber email categorycode (REQ: email)
+# PC: borrowernumber cardnumber pnr email categorycode
+
+my $patrons = Koha::Patrons->search({
+    updated_on => {'>', $dtf->format_datetime($date) },
+    categorycode => {-in => ["EX", "UX", "FR", "SR", "FX"]}
+});
+
+use Data::Dumper;
+
+open(EOP_USERS, ">eop_users.txt");
+open(PC_USERS, ">pc_users.txt");
+
+foreach my $patron (@{$patrons->unblessed}) {
+    print_eop(EOP_USERS, $patron);
+    print_pc(PC_USERS, $patron);
+}
+
+close(PC_USERS);
+close(EOP_USERS);
+
+sub print_eop {
+    my ($fp, $patron) = @_;
+
+    return if(!$patron->{email} || $patron->{email} =~ /^\s*$/);
+
+    my @row = ($patron->{borrowernumber},
+        $patron->{cardnumber},
+        $patron->{email},
+        $patron->{categorycode});
+    print $fp join("\t", @row)."\n";
+}
+
+sub print_pc {
+    my ($fp, $patron) = @_;
+
+    my $patronobj = Koha::Patrons->search({borrowernumber => $patron->{borrowernumber}})->next;
+    my $pnrobj = $patronobj->get_extended_attribute("PNR");
+    my $pnr = "";
+    if ($pnrobj) {
+        $pnr = $pnrobj->attribute;
+    }
+
+    my @row = ($patron->{borrowernumber},
+        $patron->{cardnumber},
+        $pnr,
+        $patron->{email},
+        $patron->{categorycode});
+    print $fp join("\t", @row)."\n";
+}
+
